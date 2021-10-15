5190c8cdb9f60172d6a3d801095d5499b5929512
thomasjohns <thomasjohns@fastmail.com>
2021-10-15 11:33:24 -0700

    experiment with fastapi startup and shutdown events

--- api/api/app_factory.py
+++ api/api/app_factory.py
@@ -10,8 +10,24 @@ def register_routers(app: FastAPIApp) -> None:
     app.include_router(demo_router)
 
 
+def register_startup_events(app: FastAPIApp) -> None:
+    # TODO: will this work?
+    @app.on_event('startup')
+    def temp_startup() -> None:
+        print('temp startup event printing')
+
+
+def register_shutdown_events(app: FastAPIApp) -> None:
+    # TODO: will this work?
+    @app.on_event('shutdown')
+    def temp_shutdown() -> None:
+        print('temp shutdown event printing')
+
+
 # TODO: add config param
 def create_app() -> FastAPIApp:
     app = FastAPIApp()
     register_routers(app)
+    register_startup_events(app)
+    register_shutdown_events(app)
     return app
